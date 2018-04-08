65174f00121ec53810f01952523acfb18d571f9b
songyuhang <1147884995@qq.com>
2018-04-08 16:05:37 +0800

    椭圆公式版本1

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,37 +3,44 @@
 #include "include/png/png.h"
 #include "rgbtopng.h"
 #include "android/log.h"
-const png_uint_32 width = 255;
-const png_uint_32 height = 255;
+const png_uint_32 width = 1000;
+const png_uint_32 height = 1000;
 unsigned char pixels[width*height*4];
 int getValue(int row,int num){
     return (row *width+num-1)*4;
 }
 void drawOval(int x,int y,int width,int height){
-    int leftTopX=x-width/2;
-    int leftTopY=y-height/2;
-    int rightTopX=x+width/2;
-    int rightTopY=y+height/2;
-    int leftButtonX=x-width/2;
-    int leftButtonY=y+height/2;
-    int rightButtonX=x+width/2;
-    int rightButtonY=y+height/2;
-    int F1=x-1;
-    int F2=x+1;
-    int TopX=x;
-    int TopY=y-height;
+    float leftTopX=x-width/2;
+    float leftTopY=y-height/2;
+    float rightTopX=x+width/2;
+    float rightTopY=y-height/2;
+    float leftButtonX=x-width/2;
+    float leftButtonY=y+height/2;
+    float rightButtonX=x+width/2;
+    float rightButtonY=y+height/2;
 
-    int a=sqrt(TopY*TopY+F1*F1);
-    int c=F1;
+    float F1=(rightTopX-leftTopX)/2;      //椭圆长半轴
+    float Y1=(rightButtonY-rightTopY)/2;  //椭圆短半轴
 
-    for(float i=x-width/2;i<=x+width/2;i++) {
-        float m  =sqrt(1.0*((pow(a, 2) - pow(c, 2))*pow(a,2)-(pow(a, 2) - pow(c, 2))*pow(i,2))/ pow(a,2));
-        __android_log_print(ANDROID_LOG_ERROR,"WJS_ERROR","%f",m);
-        int ret=getValue(i,m);
-        pixels[ret]=255;
-        pixels[ret+1]=0;
-        pixels[ret+2]=0;
-        pixels[ret+3]=255;
+
+    for(int i=0;i< sizeof(pixels);i++) {
+        float srtd = sqrt((1 - ((i - x) * (i - x)) / (F1 * F1)) * (Y1 * Y1)) + y;
+        int gg = round(srtd);
+        int hh = round(-srtd);
+        if (getValue(i, gg) < sizeof(pixels)) {
+            pixels[getValue(i, gg)] = 255;
+            pixels[getValue(i, gg) + 1] = 0;
+            pixels[getValue(i, gg) + 2] = 0;
+            pixels[getValue(i, gg) + 3] = 255;
+
+         }
+        /*if (getValue(i, hh) < sizeof(pixels)) {
+            pixels[getValue(i, hh)] = 255;
+            pixels[getValue(i, hh) + 1] = 0;
+            pixels[getValue(i, hh) + 2] = 0;
+            pixels[getValue(i, hh) + 3] = 255;
+
+        }*/
     }
 
 }
@@ -41,14 +48,11 @@ extern "C" JNIEXPORT jstring
 
 JNICALL Java_com_example_bitmapoper_MainActivity_stringFromJNI(JNIEnv *env, jobject,jstring jpath) {
     char *path=jstringTostring(env,jpath);
-    memset(pixels,0, sizeof(pixels));
-    drawOval(120,120,240,100);
-
-
-
+    memset(pixels,100, sizeof(pixels));
+    drawOval(500,500,1000,500);
 
     writePng(path,pixels,width,height,8);
     char value[2000];
     sprintf(value,"%d",getValue(1,3));
-    return env->NewStringUTF(value);
+    return env->NewStringUTF("dddd");
 }
